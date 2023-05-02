40144b74e5d221df4c9d4ce32b735a342cfa16e4
HyperNylium <davidpivik123@gmail.com>
2023-05-02 04:28:02 -0400

    uploading setup file

---
+++ setup.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Running as administrator...
+
+:: Check if running as administrator
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Already running as administrator
+) else (
+    echo Restarting as administrator...
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\runas.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~0""", "", "runas", 1 >> "%temp%\runas.vbs"
+    "%temp%\runas.vbs"
+    exit /b
+)
+
+:: Install customtkinter and pytube using pip
+echo Installing customtkinter and pytube...
+pip install customtkinter plyer requests winshell Pillow openai pytube pyttsx3 watchdog
+
+echo Installation complete.
+pause
